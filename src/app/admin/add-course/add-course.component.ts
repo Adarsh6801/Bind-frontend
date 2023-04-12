95c38944cd72b09893c839840bdcde514310fb43
Adarsh <kpadarsh6801@gmail.com>
2023-04-12 20:25:10 +0530

    course ui added

--- src/app/admin/add-course/add-course.component.ts
+++ src/app/admin/add-course/add-course.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage} from "@angular/fire/compat/storage"
 
 @Component({
   selector: 'app-add-course',
@@ -9,26 +10,44 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class AddCourseComponent implements OnInit{
 [x: string]: any;
   courseForm!: FormGroup;
+  imageFile!:File;
+  videoFile!:File;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private fb: FormBuilder,  private firestorage:AngularFireStorage) { }
 
   ngOnInit() {
-    this.courseForm = this.formBuilder.group({
-      courseName: ['', Validators.required],
+    this.courseForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      imageUrl: [null],
+      videoUrl: [null],
       program: ['', Validators.required],
       language: ['', Validators.required],
-      numberOfTopics: ['', Validators.required],
-      description: ['', Validators.required],
-      image: [''],
-      video: ['']
     });
+    
   }
+  imageUpload(event:any)
+  {    
+    this.imageFile = event.target.files[0];
+  }
+  videoUpload(event:any)
+  {
+    this.videoFile = event.target.files[0];
+  }
+ async onSubmit() {
 
-  onSubmit() {
-    if (this.courseForm.invalid) {
-      return;
-    }
     // Handle form submission
+    const course_image_path=`courseImage/${this.imageFile.name}`
+    const course_video_path=`courseVideo/${this.videoFile.name}`
+
+    const imageUpload=await this.firestorage.upload(course_image_path,this.imageFile)
+    const imageurl= await imageUpload.ref.getDownloadURL()
+    const videoUpload=await this.firestorage.upload(course_video_path,this.videoFile)
+    const videoUrl= await videoUpload.ref.getDownloadURL()
+    this.courseForm.patchValue({
+      imageurl:imageurl,
+      videoUrl:videoUrl
+    })
     console.log(this.courseForm.value);
   }
 }
---
+++ src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    
+
+     firebaseConfig: {
+      apiKey: "AIzaSyDYaryCYhSUMUCfDoujrIIiNCjA67KH8nk",
+      authDomain: "bind-382311.firebaseapp.com",
+      projectId: "bind-382311",
+      storageBucket: "bind-382311.appspot.com",
+      messagingSenderId: "470281030037",
+      appId: "1:470281030037:web:880529ccddf7252f258e9c",
+      measurementId: "G-71LCZNFCD2"
+      }
+  };
